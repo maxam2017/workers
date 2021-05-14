75100272727d83d6289d0a6f28614866e792c48f
maxam2017 <whk0313@gmail.com>
2021-05-14 22:37:50 +0800

    :sparkles: implement pick command

--- scripts/wrangler.sh
+++ scripts/wrangler.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 . .env
 
-if [ -z "$2" ];then
+if [ -z "$1" ];then
     echo "Your package name 🥺"
     read package_name
 else
-    package_name=$2
+    package_name=$1
 fi
 
 echo "✨ Start $package_name..."
-cd packages/$package_name && CF_ACCOUNT_ID=$CF_ACCOUNT_ID wrangler $1 $3
+
+cd packages/$package_name && CF_ACCOUNT_ID=$CF_ACCOUNT_ID wrangler "${@:2}" --stream
+# lerna run --scope $package_name "${@:2}" --stream
